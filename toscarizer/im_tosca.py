057b089a913f4843890234c1a33a097ca4d40673
micafer <micafer1@upv.es>
2022-09-08 08:22:10 +0200

    Fix script path

--- toscarizer/im_tosca.py
+++ toscarizer/im_tosca.py
@@ -135,7 +135,7 @@ def get_service(component, next_items, prev_items, resources, containers, oscar_
         "properties": {
             "name": component,
             "image": get_image_url(component, resources, containers),
-            "script": "script.sh",
+            "script": "/opt/%s/script.sh" % component,
             "input": [{
                 "storage_provider": "minio",
                 "path": "%s/input" % component if not prev_items else "%s/intermediate" % prev_items[0]  # assume only one prev
