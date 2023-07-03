60a9635bc0537b4eab7b0720c4f5814ce7a46b82
Miguel Caballer <micafer1@upv.es>
2023-07-03 08:33:09 +0200

    Add missing template files in setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     long_description='TOSCARIZER',
     url='https://gitlab.polimi.it/grycap/toscarizer',
     python_requires='>=3.7',
-    package_data={"": ['Dockerfile.template', 'Dockerfile.aws.template', 'script.sh', 'oscar.yaml',
-                       'oscar_wn.yaml', 'oscar_wn_elastic.yaml', 'oscar_elastic.yaml']},
+    package_data={"": ['Dockerfile.template', 'Dockerfile.aws.template', 'script.sh', 'start.sh', 'oscar.yaml',
+                       'oscar_wn.yaml', 'oscar_wn_elastic.yaml', 'oscar_elastic.yaml', 'telegraf.conf']},
     packages=find_packages(),
     install_requires=['PyYAML', 'networkx', 'docker', 'requests', 'click'],
     entry_points={
